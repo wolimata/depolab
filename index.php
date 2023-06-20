011c7ec889ea828283410d71ac55185298aa28ab
Abel Diatta <abeldiatta09@gmail.com>
2023-06-20 23:03:14 +0000

    First update index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>OURS FIRST DEPOT GIT REPOSITORY</h1>
+    <span>Wolimata Sarr</span>
+    <span>Abel Bernard Diatta</span>
+</body>
+</html>
